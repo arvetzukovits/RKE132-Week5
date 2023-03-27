13031afc55d3067c9ec7c37b567aa6eba0c5266d
arvetzukovits <arvet.zukovitsrtktk.ee>
2023-03-27 22:15:29 +0300

    add week5logicaloperators code

---
+++ Week5LogicalOperators/Program.cs
@@ -0,0 +1,16 @@
+﻿
+
+//AND - &&
+
+//true && true --> true
+//true && false --> false
+//false && true --> false
+//false & false --> false
+
+
+//OR - || (pipe)
+
+//true || true --> true
+//true || false --> true
+//false || true --> true
+//false || false --> false
